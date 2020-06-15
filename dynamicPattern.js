b5423397bd37a734940285875b88b4d6d727fd6d
Sagar Panchal <panchal.sagar@outlook.com>
2020-06-15 16:57:20 +0530

    pattern

---
+++ dynamicPattern.js
@@ -0,0 +1,71 @@
+/**
+ * Prints Dynamic pattern
+ *
+ * Command to run this script - `node .\dynamicPattern.js 4`
+ * `4` is the numbers of rows need to print
+ */
+
+/**
+ * Write triangle pattern to console
+ * @param {*} callBack     return strign to print on console
+ * @param {*} options.max  max number of iterations (rows) [(n*2)-1 when full is true]
+ * @param {*} options.pad  max characters to pad
+ * @param {*} options.full full rectangle pattern
+ */
+const writePattern = (callBack = () => "*", options = {}) => {
+  let { max, pad, full } = { max: 5, pad: 12, full: true, ...options };
+
+  if (isNaN(max)) throw new Error("Param `max` requires a numeric value");
+  if (isNaN(pad)) throw new Error("Param `pad` requires a numeric value");
+  if (typeof full !== "boolean")
+    throw new Error("Param `full` requires a boolean value");
+  if (max < 1) throw new Error("Param `max` must be >= 1");
+  if (pad < 2) throw new Error("Param `pad` must be >= 2");
+
+  if (max.length >= pad) pad = max.length * 6;
+  const padNewlines = "\n".repeat(pad / 6);
+
+  let n = 0;
+
+  for (let x = 1; x <= max; x++) {
+    void (
+      ((!(max % 2) && x % 2) || (max % 2 && !(x % 2))) &&
+      process.stdout.write(" ".repeat(pad / 2))
+    );
+
+    for (let y = 1; y <= (max - x) / 2; y++)
+      process.stdout.write(" ".repeat(pad));
+
+    for (let y = 1; y <= x; y++) {
+      const value = callBack([x, y], n++);
+      process.stdout.write(value + " ".repeat(pad - value.length));
+    }
+
+    process.stdout.write(padNewlines);
+  }
+
+  if (!full) return;
+
+  for (let x = max - 1; x >= 1; x--) {
+    void (
+      ((!(max % 2) && x % 2) || (max % 2 && !(x % 2))) &&
+      process.stdout.write(" ".repeat(pad / 2))
+    );
+
+    for (let y = 1; y <= (max - x) / 2; y++)
+      process.stdout.write(" ".repeat(pad));
+
+    for (let y = 1; y <= x; y++) {
+      const value = callBack([x, y], n++);
+      process.stdout.write(value + " ".repeat(pad - value.length));
+    }
+
+    process.stdout.write(padNewlines);
+  }
+};
+
+writePattern(([x, y], n) => `${String.fromCharCode(97 + n)}${x}`, {
+  max: process.argv[2] || 5,
+  pad: process.argv[3] || 12,
+  full: process.argv[4] === "full",
+});
